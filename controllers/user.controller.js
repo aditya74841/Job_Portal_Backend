41cb0907674d7ab8ba099875b407fe3ab3476372
Aditya Ranjan <aditya74810@gmail.com>
2024-08-08 18:55:37 +0530

    .

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -98,7 +98,7 @@ export const login = async (req, res) => {
     return res
       .status(200)
       .cookie("token", token, {
-        maxAge: 1 * 24 * 60 * 60 * 1000,
+        maxAge: 12 * 24 * 60 * 60 * 1000,
         httpsOnly: true,
         sameSite: "strict",
       })
